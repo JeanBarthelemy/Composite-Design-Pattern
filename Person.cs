785f3bbb58743a8e22678446094c0e26fc30c708
JeanBarthelemy <58735903+JeanBarthelemy@users.noreply.github.com>
2020-02-17 17:10:47 +0100

    solution

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite_Design_Pattern_Quête_2
+{
+    public class Person
+    {
+        public List<Person> descendants1 { get; } = new List<Person>();
+        public string name { get; set; }
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            string description = name;
+            foreach (Person descendant in descendants1)
+            {
+                description = description + "\n-" + descendant;
+            }
+            return description;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Composite_Design_Pattern_Quête_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person eric = new Person("Eric");
+            Person jean = new Person("Jean");
+            Person charlie = new Person("Charlie");
+            eric.descendants1.Add(jean);
+            eric.descendants1.Add(charlie);
+            Console.WriteLine(eric);
+
+        }
+    }
+}
